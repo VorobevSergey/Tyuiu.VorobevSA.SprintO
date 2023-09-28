ce9e3eba58348bdf2a35a50e698a6a7afed5a4e0
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-09-28 17:17:40 +0500

    Task2

---
+++ Tyuiu.VorobevSA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VorobevSA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VorobevSA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Сергей";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Сергей", res);
+        }
+    }
+}
