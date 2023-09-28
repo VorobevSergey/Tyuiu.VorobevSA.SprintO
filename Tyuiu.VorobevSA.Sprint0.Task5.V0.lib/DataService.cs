fcff659f5c89894f09fe801b12e3b0db47c918f0
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-09-28 18:12:01 +0500

    Task5

---
+++ Tyuiu.VorobevSA.Sprint0.Task5.V0.lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VorobevSA.Sprint0.Task5.V0.lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.VorobevSA.Sprint0.Task5.V0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VorobevSA.Sprint0.Task5.V0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VorobevSA.Sprint0.Task5.V0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f71ea5cd-2530-417b-9976-3bc245d416a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
