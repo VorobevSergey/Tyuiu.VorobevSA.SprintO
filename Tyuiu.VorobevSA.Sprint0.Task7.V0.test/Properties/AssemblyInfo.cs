a56542a19f04a27373c1f21a64f5662366aa9328
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-09-28 19:24:34 +0500

    Task7

---
+++ Tyuiu.VorobevSA.Sprint0.Task7.V0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VorobevSA.Sprint0.Task7.V0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VorobevSA.Sprint0.Task7.V0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24c1fa69-54d9-488c-a5be-a33e088657cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
