b08d3842a57a7373a0ed64e385b663390c85e311
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-09-28 16:58:31 +0500

    Task1

---
+++ Tyuiu.VorobevSA.Sprint0.Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VorobevSA.Sprint0.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow,World!");
+            string name = "Сергей";
+            int age = 21;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год");
+            Console.ReadKey();
+        }
+    }
+}
